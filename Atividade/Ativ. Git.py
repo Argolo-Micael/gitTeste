9bf10218669bb4a547c3f3dbda8787dd931bdb52
Argolo-Micael <argolomicael@gamail.com>
2025-02-19 16:52:42 -0300

    Código finalizado

--- Atividade/Ativ. Git.py
+++ Atividade/Ativ. Git.py
@@ -30,9 +30,55 @@ elif Resposta == "N" or Resposta == "n":
 else:
     print(F"VOCÊ NÃO TEM ESCOLHA {Nome} HAHAHAHAHAHA")
 
-print('O JOGO É JOKENPÔ')
-print('[ 0 ] PEDRA')
-print('[ 1 ] PAPEL')
-print('[ 2 ] TESOURA')
-escolha_comp = randint(0,2)
-rscolha_jogador = int(input('DIGITE A OPÇÃO DESEJADA: '))
+while True:
+    print('O JOGO É JOKENPÔ')
+    print('[ 0 ] PEDRA')
+    print('[ 1 ] PAPEL')
+    print('[ 2 ] TESOURA')
+    Computador = randint(0,2)
+    Jogador = int(input('DIGITE A OPÇÃO DESEJADA: '))
+
+    # ESCOLHA MAQUINA
+    if Computador == 0:
+        print("___________________________________")
+        print("O COMPUTADOR ESCOLHEU PEDRA")
+    elif Computador == 1:
+        print("___________________________________")
+        print("O COMPUTADOR ESCOLHEU PAPEL")
+    else:
+        print("___________________________________")
+        print("O COMPUTADOR ESCOLHEU TESOURA")
+
+    # ESCOLHA JOGADOR
+    if Jogador == 0:
+        print("O JOGADOR ESCOLHEU PEDRA")
+        print("___________________________________")
+        print("")
+    elif Jogador == 1:
+        print("O JOGADOR ESCOLHEU PAPEL")
+        print("___________________________________")
+        print("")
+    else:
+        print("O JOGADOR ESCOLHEU TESOURA")
+        print("___________________________________")
+        print("")
+
+
+    if Computador == Jogador:
+        print("O AMBOS ESCOLHERAM A MESMA OPÇÃO O JOGO EMPATOU!")
+
+    elif Computador == 0 and Jogador == 1:
+        print(f"vOCÊ GANHOU {Nome} :( ")
+    elif Computador == 2 and Jogador == 0:
+        print(f"vOCÊ GANHOU {Nome} :( ")
+    elif Computador == 1 and Jogador == 2:
+        print(f"vOCÊ GANHOU {Nome} :( ")
+
+    else: 
+        print(f"VOCÊ PERDEU {Nome} MUITO RUIM HAHAHAHAHAHAHA!")
+
+    print("QUER PARAR? ")
+    Resposta_2 = input("Sim [Y]     Não [N]  ")
+
+    if Resposta_2 == "Y" or Resposta_2 == "y":
+        break
