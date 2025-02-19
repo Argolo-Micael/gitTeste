2eabc5652e8cc0b203428f063b0da2f27589bee7
EndrewFerreira <ferreira10980@gmail.com>
2025-02-19 15:34:59 -0300

    add hora no print

--- Atividade/Ativ. Git.py
+++ Atividade/Ativ. Git.py
@@ -3,9 +3,9 @@ idade = int(input('digite sua idade: '))
 Hora = input("Informe a hora atual: ")
 
 if Hora <= "12:00":
-    print (f"Bom Dia! {Nome}, sua idade atual é {idade} anos")
+    print (f"Bom Dia! {Nome}, sua idade atual é {idade}anos e a hora é {Hora}")
 elif Hora <= "18:00":
-    print (f"Boa Tarde! {Nome}, sua idade atual é {idade} anos")
+    print (f"Boa Tarde! {Nome}, sua idade atual é {idade} anos e a hora é {Hora}")
 else: 
-    print (f"Boa Noite! {Nome}, sua idade atual é {idade} anos")
+    print (f"Boa Noite! {Nome}, sua idade atual é {idade} anos e a hora é {Hora}")
     
