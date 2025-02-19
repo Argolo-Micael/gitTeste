a6295bffb3e412eae13280f73c83d1784e15df19
EndrewFerreira <ferreira10980@gmail.com>
2025-02-19 16:00:44 -0300

    alteração correta da data e add print

--- Atividade/Ativ. Git.py
+++ Atividade/Ativ. Git.py
@@ -6,5 +6,13 @@ Agora = datetime.now()
 Data = Agora.strftime("%d/%m/%Y")
 Hora = Agora.strftime("%H:%M")
 
-
+print(f'seu nome é {Nome}, sua idade é {idade}')
 print(f"Data: {Data} Hora: {Hora}")
+if Hora <= '12:00':
+    print(f'bom dia {Nome}')
+elif Hora <= '18:00':
+    print(f'boa tarde {Nome}')
+else:
+    print(f'boa noite {Nome}')
+
+
